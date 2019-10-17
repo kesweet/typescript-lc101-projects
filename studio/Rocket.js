608af9fc008ceeb31e9af8bb18e681ada28949bd
Katherine Sweet <katherine.sweet@slu.edu>
2019-10-17 13:04:12 -0500

    final commit of class objects

---
+++ studio/Rocket.js
@@ -0,0 +1,47 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var totalMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            totalMass += items[i].massKg;
+        }
+        return totalMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var first = this.sumMass(this.astronauts);
+        var second = this.sumMass(this.cargoItems);
+        return first + second;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
